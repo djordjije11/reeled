d213c7b81b38d469bdd9888974235f51f0d0169e
djordjije11 <djordjijeradovic24@gmail.com>
2025-08-30 16:30:58 +0200

    stash

--- services/legacy-connector-service/src/main/java/io/github/djordjije11/reeled/legacyconnector/domain/AuthorLegacyAuthorSyncEntry.java
+++ services/legacy-connector-service/src/main/java/io/github/djordjije11/reeled/legacyconnector/domain/AuthorLegacyAuthorSyncEntry.java
@@ -67,6 +67,7 @@ public class AuthorLegacyAuthorSyncEntry {
 
         this.id = id;
         this.legacyAuthorData = legacyAuthorData;
+        this.legacy = true;
     }
 
     public void sync(AuthorData authorData, boolean legacy, LegacyClient legacyClient) {
@@ -90,6 +91,10 @@ public class AuthorLegacyAuthorSyncEntry {
     public void sync(LegacyAuthorData legacyAuthorData, AuthorServiceClient authorServiceClient) {
         Assert.notNull(legacyAuthorData, "legacyAuthorData must not be null");
         Assert.notNull(authorServiceClient, "authorServiceClient must not be null");
+
+        if (!legacy) {
+            return;
+        }
 
         if (legacyAuthorData.equals(this.legacyAuthorData)) {
             return;
