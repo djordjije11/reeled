dc195e0c655e662c4606304f931cb98c76809ceb
djordjije11 <djordjijeradovic24@gmail.com>
2025-09-06 18:27:47 +0200

    Trying out transaction properties of Debezium connector in reeled-legacy

--- services/reeled-legacy/scripts/wait-and-setup-backing-services.sh
+++ services/reeled-legacy/scripts/wait-and-setup-backing-services.sh
@@ -16,7 +16,9 @@ curl -L https://maven.pkg.github.com/djordjije11/reeled/io.github.djordjije11.re
  -o reeled-kafka-admin.jar
 java -jar reeled-kafka-admin.jar localhost:9092 reeledlegacy-default.public.post \
   reeledlegacy-default.public.author \
-  reeledlegacy-default.public.post_category
+  reeledlegacy-default.public.post_category \
+  reeledlegacy-default.public.author_type \
+  reeledlegacy-default.transaction
 rm -rf reeled-kafka-admin.jar
 
 # Setup Debezium Postgres connector
@@ -32,11 +34,15 @@ curl -i -X POST -H "Accept: application/json" -H "Content-Type: application/json
     "database.user": "reeled_legacy",
     "database.password": "reeled_legacy_password",
     "database.server.name": "reeledlegacy",
-    "table.include.list": "public.post,public.author,public.post_category",
-    "transforms": "RenameNamespace",
+    "table.include.list": "public.post,public.author,public.post_category,public.author_type",
+    "provide.transaction.metadata": "true",
+    "transforms": "RenameNamespace,RenameTxn",
     "transforms.RenameNamespace.type": "org.apache.kafka.connect.transforms.RegexRouter",
     "transforms.RenameNamespace.regex": "reeledlegacy\\.public\\.(.*)",
     "transforms.RenameNamespace.replacement": "reeledlegacy-default.public.$1",
+    "transforms.RenameTxn.type": "org.apache.kafka.connect.transforms.RegexRouter",
+    "transforms.RenameTxn.regex": "reeledlegacy\\.transaction",
+    "transforms.RenameTxn.replacement": "reeledlegacy-default.transaction",
     "key.converter": "io.confluent.connect.avro.AvroConverter",
     "key.converter.schema.registry.url": "http://schema-registry:8081",
     "key.converter.key.subject.name.strategy": "io.confluent.kafka.serializers.subject.TopicRecordNameStrategy",
