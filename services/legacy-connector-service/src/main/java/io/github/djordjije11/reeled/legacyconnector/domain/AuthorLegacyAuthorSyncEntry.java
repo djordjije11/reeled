6fa79452133341cf3e5c2bd0a3c3ebce34a27971
djordjije11 <djordjijeradovic24@gmail.com>
2025-09-12 19:25:36 +0200

    remove legacy

--- services/legacy-connector-service/src/main/java/io/github/djordjije11/reeled/legacyconnector/domain/AuthorLegacyAuthorSyncEntry.java
+++ services/legacy-connector-service/src/main/java/io/github/djordjije11/reeled/legacyconnector/domain/AuthorLegacyAuthorSyncEntry.java
@@ -65,7 +65,6 @@ public class AuthorLegacyAuthorSyncEntry {
 
         this.id = id;
         this.legacyAuthorData = legacyAuthorData;
-        this.legacy = true;
 
         syncCreatedOnLegacy(authorServiceClient);
     }
@@ -88,10 +87,6 @@ public class AuthorLegacyAuthorSyncEntry {
     public void sync(LegacyAuthorData legacyAuthorData, AuthorServiceClient authorServiceClient) {
         Assert.notNull(legacyAuthorData, "legacyAuthorData must not be null");
         Assert.notNull(authorServiceClient, "authorServiceClient must not be null");
-
-        if (!legacy) {
-            return;
-        }
 
         if (legacyAuthorData.equals(this.legacyAuthorData)) {
             return;
